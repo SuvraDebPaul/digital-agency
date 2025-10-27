6fcf1e50112971a7e5ea77ca7e50f91e3391bef5
Suvra Deb Paul <suvra.deb.paul@gmail.com>
2025-10-27 20:26:46 +0600

    NavBar Design Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
   return (
-    <div>
-      <h1>App Component</h1>
+    <div className="dark:bg-black relative ">
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
   );
 };
